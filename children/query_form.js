68eccac4e5a0f47e3fbbc9b649ecf7d47f48c7a0
seanpatrickmoran <seanpatrickmoran@gmail.com>
2024-12-25 01:00:37 -0800

    Query Selection Improved

--- children/query_form.js
+++ children/query_form.js
@@ -45,8 +45,8 @@ function tailOfSQLClick(){
 };
 
 
-function query_with_textbox(keyname){
-    var search = document.getElementById('sqlite3-query').value;
+function query_with_textbox(keyname,route){
+    var search = document.getElementById(route).value;
     console.log(keyname, search);
     var names = functionMapped[keyname](search)
     console.log(names);
@@ -54,7 +54,7 @@ function query_with_textbox(keyname){
     let nameString = names.map((elem) => {
         return elem.name
     }).join("<option />");
-    // console.log(nameString);
+    console.log(nameString);
     divNames.innerHTML = "<option />" + nameString;
 };
 
@@ -69,57 +69,29 @@ function queryToSelectbox(search){
 
 
 
-
-
-
-
-// function ShowImage(keyname){
-//     var search = document.getElementById('sqlite3-query').value;
-//     // var names = window.api.getNames(search);
-//     // var names = window.api.getDataset(search);
-//     var names = functionMapped[keyname](search);
-//     let divNames = document.getElementById("names");
-//     let nameString = names.map((elem) => {
-//         return elem.name
-//     }).join("<option />");
-//     divNames.innerHTML = nameString;
-//     console.log(nameString);
-// }
-
-
-
-
-// document.addEventListener('DOMContentLoaded', async () => {
-//     defaultRender();
-// });
-
-
-
 document.addEventListener('DOMContentLoaded', async () => {
     var search = document.getElementById('field-select').value;
     var nameString = queryToSelectbox(search);
     var divNames = document.getElementById("avail-field-select");
-    divNames.innerHTML = "<option /> ---Scroll for all--- </option><option />" + nameString;
+    divNames.innerHTML = "<option /> ---Scroll for all results--- </option><option />" + nameString;
 
 });
 
-document.querySelector('#tail-sql').addEventListener('click', async () => {
-    tailOfSQLClick()
-});
+
+// document.querySelector('#tail-sql').addEventListener('click', async () => {
+//     tailOfSQLClick()
+// });
 
 
 document.querySelector('#queryBtn').addEventListener('click', async () => {
     var fieldSelect = document.getElementById("field-select");
     var value = fieldSelect.value;
     var text = fieldSelect.options[fieldSelect.selectedIndex].text;
-    if (document.getElementById("avail-field-select").value === " ---Scroll for all--- ") {
-        /*
-        Query and write to 
-        */
+    console.log(document.getElementById("avail-field-select").value);
+    if (document.getElementById("avail-field-select").value === "---Scroll for all results---") {
+        query_with_textbox(text,'sqlite3-query');
     } else {
-        /*
-        Otherwise, there's a selected option, query these instead.
-        */
+        query_with_textbox(text,"avail-field-select");
     };
 
 });
@@ -133,31 +105,5 @@ document.getElementById('field-select').addEventListener('change', async () => {
         return elem[search]
     }).join("<option />");
     let divNames = document.getElementById("avail-field-select");
-    divNames.innerHTML = "<option /> ---Scroll for all--- </option><option />" + nameString;
+    divNames.innerHTML = "<option /> ---Scroll for all results--- </option><option />" + nameString;
 });
-
-// document.querySelector('#inspectBtn').addEventListener('click', async () => {
-//     var colOfSelectedOpt = document.getElementById("names").selectedOptions; //This is how we call selected values
-//     var values = [];
-//     for(var i=0;i<colOfSelectedOpt.length;i++) {
-//          values.push(colOfSelectedOpt[i].value);
-//     }
-//     console.log(values);
-//     return values;
-// });
-
-
-// document.querySelector('#resetBtn').addEventListener('click', async () => {
-//     // defaultRender()
-// });
-
-// document.querySelector('#viewBtn').addEventListener('click', async () => {
-//     var colOfSelectedOpt = document.getElementById("names").selectedOptions; //This is how we call selected values
-//     var values = [];
-//     for(var i=0;i<colOfSelectedOpt.length;i++) {
-//          values.push(colOfSelectedOpt[i].value);
-//     }
-//     return values;
-// });
-
-
